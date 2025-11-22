6f7f81a63e3f44b2a3a50541f1411c4177596320
Maged798 <maged.ahmed4@outlook.com>
2025-11-23 00:16:06 +0200

    Movie module

--- SW_Testing_Team8/src/main/java/org/Models/Movie.java
+++ SW_Testing_Team8/src/main/java/org/Models/Movie.java
@@ -1,4 +1,24 @@
 package org.Models;
 
+import java.util.Vector;
+
 public class Movie {
+    private  String title;
+    private String movieID;
+    private Vector<String> genres;
+
+    public Movie(String title, String movieID, Vector<String> genres) {
+        this.title = title;
+        this.movieID = movieID;
+        this.genres = genres;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getMovieID() {
+        return movieID;
+    }
+    public Vector<String> getGenres() {
+        return genres;
+    }
 }
