685fec9e88c47facf037217dcd69e986bd759dcb
Andro <Andro_milad22@yahoo.com>
2025-11-23 20:31:02 +0200

    the validator files are created and the ExceptionHandler is implemented

---
+++ SW_Testing_Team8/src/main/java/org/example/ExceptionHandler.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ExceptionHandler {
+    private List<String> errorLog;
+
+    public ExceptionHandler() {
+        errorLog = new ArrayList<String>();
+    }
+
+    public void throwValidationError(String message) {
+        errorLog.add(message);
+        throw new IllegalArgumentException(message);
+    }
+
+
+    public void logError(String message) {
+        errorLog.add(message);
+    }
+
+    public List<String> getErrorLog() {
+        return Collections.unmodifiableList(errorLog);
+    }
+
+    public void clearErrorLog() {
+            errorLog.clear();
+    }
+
+    public void printErrorLog() {
+        for(String error : errorLog) {
+            System.out.println(error);
+        }
+    }
+}
