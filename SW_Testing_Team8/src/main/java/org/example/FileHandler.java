f0bf48aded0334b9f21b5ea8479f89a74bd03194
Maged798 <maged.ahmed4@outlook.com>
2025-11-25 14:13:19 +0200

    FileHandler

--- SW_Testing_Team8/src/main/java/org/example/FileHandler.java
+++ SW_Testing_Team8/src/main/java/org/example/FileHandler.java
@@ -14,22 +14,21 @@ public class FileHandler {
         BufferedReader reader;
         try{
             reader = new BufferedReader(new FileReader(filePathMovie));
-            String line =  reader.readLine();
+            String line;
             String[] splittedLine;
             String title;
             String MovieID;
-            ArrayList<String> genres =  new ArrayList<>();
             while((line  = reader.readLine()) != null){
                 splittedLine = line.split(",");
                 title = splittedLine[0];
                 MovieID = splittedLine[1];
                 line  = reader.readLine();
                 splittedLine = line.split(",");
+                ArrayList<String> genres =  new ArrayList<>();
                 for (String genre : splittedLine) {
                     genres.add(genre);
                 }
                 movies.add(new Movie(title, MovieID, genres));
-                genres = null;
             }
         }
         catch(IOException e){
@@ -42,22 +41,21 @@ public class FileHandler {
         BufferedReader reader;
         try{
             reader = new BufferedReader(new FileReader(filePathUser));
-            String line =  reader.readLine();
+            String line;
             String[] splittedLine;
             String UserName;
             String UserID;
-            ArrayList<String> likedMoviesID =  new ArrayList<>();
             while((line  = reader.readLine()) != null){
                 splittedLine = line.split(",");
                 UserName = splittedLine[0];
                 UserID = splittedLine[1];
                 line  = reader.readLine();
                 splittedLine = line.split(",");
+                ArrayList<String> likedMoviesID =  new ArrayList<>();
                 for (String likedMovieID : splittedLine) {
                     likedMoviesID.add(likedMovieID);
                 }
                 users.add(new User(UserName, UserID, likedMoviesID));
-                likedMoviesID = null;
             }
         }
         catch(IOException e){
